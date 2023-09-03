c8161af59124bbb192a633655d6d330880f01956
Z1n0u <taztala1@gmail.com>
2023-09-03 22:25:20 +0100

    completed the right side of the banner and i'm start working on the navbar effect

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,7 @@
 import './Banner.css'
 import Button from '../Button/Button'
+import Gamecard from '../../assets/images/banner-image.jpg'
+import caption from '../../assets/images/caption-dec.png'
 
 
 const Banner = () => {
@@ -7,12 +9,20 @@ const Banner = () => {
     <div className='banner'>
         <div className='banner_div left'>
             <h6 className='banner_text_welcome'>welcome to lugx</h6>
+            <div className='banner_left_cap_text_conatiner'>
             <h2 className='banner_text_title'>best gaming site ever!</h2>
+            <img src={caption} className='banner_left_cap'></img>
+            </div>
             <p className='banner_text_subtitle'>LUGX Gaming is a games key store where you find the best games with the best price possible.</p>
             <div className='banner_search'>
                 <input type='text' placeholder='Type Something'></input>
                 <Button/>
             </div>
+        </div>
+        <div className='banner_div right'>
+            <img src={Gamecard} id='banner_img'></img>
+            <span className='price'>20$</span>
+            <span className='promo'>-40%</span>
         </div>
     </div>
     )
